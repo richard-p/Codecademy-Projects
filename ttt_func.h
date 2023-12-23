5d9c55c89d6b3b3d801b9ff00297c1bc0b412655
Richard P <richard.perdomo19@gmail.com>
2023-12-22 23:04:37 -0800

    Delete ttt_func.h

--- ttt_func.h
+++
@@ -1,9 +0,0 @@
-void display_board(std::vector<char> row1, std::vector<char> row2, std::vector<char> row3);
-
-bool check_valid_input(char in1, int in2);
-
-int ugly_mapping(int position);
-
-int locate_tile(char xotile, int position);
-
-int checkWinner(std::vector<char> row1, std::vector<char> row2, std::vector<char> row3);
